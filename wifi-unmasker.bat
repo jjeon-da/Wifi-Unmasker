8625a5fef79ad587a4c7b8993ea8e48103722331
jjeonda <79075756+jjeon-da@users.noreply.github.com>
2023-12-08 19:08:42 -0500

    Update wifi-unmasker.bat

--- wifi-unmasker.bat
+++ wifi-unmasker.bat
@@ -1,3 +1,4 @@
+#tool author:  jjeonda    https://github.com/jjeon-da
 @echo off
 for /f "tokens=4,* skip=4" %%a in ('netsh wlan show profile') do ( for /f "tokens=*" %%c in ('netsh wlan show profile "%%b" key^=clear') do ( for /f "tokens=3,*" %%d in ('echo %%c^| find /i "Key Content"') do ( echo [SSID: %%b] [PASSWORD: %%e] ) ) )
 pause
